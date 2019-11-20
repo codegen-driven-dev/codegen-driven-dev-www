0bee858fcfdece6fc07f3bf81f0cf33b21cd94db
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-11-20 11:07:04 +1100

    Regenerate scaffold, concentrate on flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
-import { SvgViewerComponent } from './svg-viewer/svg-viewer.component';
 
 @NgModule({
   declarations: [
@@ -13,9 +10,7 @@ import { SvgViewerComponent } from './svg-viewer/svg-viewer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/slash/slash.module.ts
+++ src/app/slash/slash.module.ts
@@ -3,19 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material';
 
-import { SvgViewerModule } from '../svg-viewer/svg-viewer.module';
-import { slashRoutes } from './slash.routes';
 import { SlashComponent } from './slash.component';
+import { slashRoutes } from './slash.routes';
 
 
 @NgModule({
   declarations: [SlashComponent],
   imports: [
-    CommonModule, RouterModule.forChild(slashRoutes),
-    FlexLayoutModule, MatCardModule,
-    SvgViewerModule
+    CommonModule, RouterModule, RouterModule.forChild(slashRoutes),
+    FlexLayoutModule
   ]
 })
 export class SlashModule {}
