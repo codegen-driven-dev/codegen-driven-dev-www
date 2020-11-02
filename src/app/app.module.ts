d69fe2450a6da6ca24533b51a6d42774fee67e08
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-11-02 18:00:04 +1100

    [src/app/slash/slash.component.html] Fix Safari and mobile support; [package-lock.json] Regenerate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NavbarModule } from './navbar/navbar.module';
 import { SlidesComponent } from './slides/slides.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { SlidesComponent } from './slides/slides.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavbarModule
+    NavbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
